ef56bbbd312a2854c7af48128ce430a9934e76bd
sovalke <sonja.valkeinen@gmail.com>
2020-12-07 19:06:04 +0200

    Teht. 2

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -3,16 +3,35 @@ package statistics;
 import statistics.matcher.*;
 
 public class Main {
+
     public static void main(String[] args) {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
-          
-        Matcher m = new And( new HasAtLeast(5, "goals"),
-                             new HasAtLeast(5, "assists"),
-                             new PlaysIn("PHI")
+
+        Matcher m = new And(new HasAtLeast(5, "goals"),
+                new HasAtLeast(5, "assists"),
+                new PlaysIn("PHI")
         );
-        
+
+        // All
+        System.out.println(stats.matches(new All()).size());
+
+        // HasAtLeast
+        System.out.println("HasAtLeast");
+        Matcher m2 = new And(new Not(new HasAtLeast(1, "goals")), new PlaysIn("NYR"));
+        for (Player player : stats.matches(m2)) {
+            System.out.println(player);
+        }
+
+        //HasFewerThan
+        System.out.println("HasFewerThan");
+        Matcher m3 = new And(new HasFewerThan(1, "goals"), new PlaysIn("NYR"));
+        for (Player player : stats.matches(m3)) {
+            System.out.println(player);
+        }
+
+        System.out.println("THE REST");
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
